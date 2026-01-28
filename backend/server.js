ccbd6c3168eaaa84833b7b8af50ef0cdb028498c
cu-deve <chhonoudomcanva@gmail.com>
2026-01-29 03:36:52 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -62,3 +62,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+
+app.get("/", (req, res) => {
+  res.send("🚀 YouTube Downloader API is running");
+});
